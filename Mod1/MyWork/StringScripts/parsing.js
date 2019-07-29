6acbf3dbabc7d546a65a0797ad5495f15c228c11
Sudesh Pamidi <sudesh.pamidi@gmail.com>
2019-07-29 15:45:09 -0400

    Workded on Lab scripts

---
+++ Mod1/MyWork/StringScripts/parsing.js
@@ -0,0 +1,41 @@
+//Description: This script is lab execises for substring, substr and slice
+//Author: Sudesh Pamidi
+"use strict"
+
+//let name = "Brenda Lynn Grace";
+
+function parseAndDisplayName(fullname) {
+
+    let firstName = "",
+        lastName = "",
+        middleName = "";
+    let pos = fullname.indexOf(" ");
+    let pos2 = fullname.lastIndexOf(" ");
+
+    if (pos != -1) {
+        firstName = fullname.substring(0, pos);
+        if (pos != pos2) {
+            middleName = fullname.substring(pos + 1, pos2);
+            lastName = fullname.substring(pos2 + 1);
+        } else {
+
+            lastName = fullname.substring(pos + 1);
+        }
+
+    } else {
+        firstName = fullname;
+        lastName = "";
+    }
+
+
+    console.log("First Name: " + firstName);
+
+    if (middleName != "")
+        console.log("Middle Name: " + middleName);
+    if (lastName != "")
+        console.log("Last Name: " + lastName);
+}
+
+parseAndDisplayName("Cher");
+parseAndDisplayName("Lan Auston");
+parseAndDisplayName("Siddalee Lynn Grace");
